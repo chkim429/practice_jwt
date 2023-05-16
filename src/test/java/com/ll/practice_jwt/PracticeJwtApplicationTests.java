63a927d0253ff948002b663d0ea96f98e227ac74
chkim429 <ckdgjs000@naver.com>
2023-05-16 16:10:51 +0900

    초기 세팅

---
+++ src/test/java/com/ll/practice_jwt/PracticeJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.practice_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
